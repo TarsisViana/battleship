f259a4badd0a1d31cdaf8ee52acf20aac6c3fd2f
TarsisViana <159029529+TarsisViana@users.noreply.github.com>
2024-07-05 15:36:46 +0100

    Added a incial receiveAttack func and comments

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -6,6 +6,7 @@ export default class GameBoard {
     this.addShip(3, [0, 0]);
   }
 
+  //board is an object indexed by the position of the tiles
   makeBoard() {
     let board = {};
     for (let i = 0; i < 8; i++) {
@@ -19,15 +20,29 @@ export default class GameBoard {
 
   addShip(size, pos) {
     let ship = new Ship(size);
+    //each tile has a link to its ship, if one is placed there
     this.board[pos].ship = ship;
 
     let [posx, posy] = pos;
 
+    //from the inicial point link the correct adjacent tiles to
+    //the ship, depending if horizontal or vertical
     for (let i = 1; i < size; i++) {
       posx++;
 
       let newP = [posx, posy];
       this.board[newP].ship = ship;
+    }
+  }
+
+  receiveAttack(pos) {
+    //each tile can only be attacked once
+    if (this.board[pos].hit) return;
+    if (this.board[pos].ship) {
+      this.board[pos].ship.hit();
+      this.board[pos].hit = true;
+    } else {
+      this.board[pos].hit = true;
     }
   }
 }
@@ -36,5 +51,6 @@ class Tile {
   constructor(pos, ship = null) {
     this.pos = pos;
     this.ship = ship;
+    this.hit = false;
   }
 }
